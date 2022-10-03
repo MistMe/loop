e939cd46c65cdb65185ebef35094e920618cb3d9
Lyod Rhussel Torrenueva <111472869+MistMe@users.noreply.github.com>
2022-10-03 10:32:09 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,15 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 5; i != 0; i--) {
+        //For Loop
+        for (int firstnameCount = 5; firstnameCount != 0; firstnameCount--) {
             System.out.println("Lyod Rhussel");
         }
         
+        //While Loop
         int nicknameCount = 5;
         while (nicknameCount != 0) {
             System.out.println("lyod");
             nicknameCount--;
   }
+        //Do-While Loop
         int surnameCount = 5;
         do {
             System.out.println("Torrenueva");
